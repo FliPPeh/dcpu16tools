22a6abcad29d1606d59e87b9ce8d28385d690923
Lukas Niederbremer <Lukas.Niederbremer@hs-osnabrueck.de>
2012-04-10 20:07:20 +0200

    Make the linked list sources fit in with the rest (written years before this)

--- linked_list.c
+++ linked_list.c
@@ -1,24 +1,32 @@
-/******************************************************************************
+/*
+ * Copyright (c) 2012
  *
- *  Simple linked list implementation (linked_list.c)
- *  ---
- *  Construct, free and maintain a singly linked list
+ * This file is part of dcpu16tools
  *
- *  Created: 06.01.2011 20:38:07
- *  Author:  Lukas Niederbremer
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- ******************************************************************************/
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 #include <stdlib.h>
 #include <string.h>
 
 #include "linked_list.h"
 
 
-linked_list *list_create()
-{
+linked_list *list_create() {
     linked_list *list = malloc(sizeof(linked_list));
-    if (list)
-    {
+
+    if (list) {
         list->root = NULL;
         list->length = 0;
     }
@@ -26,36 +34,30 @@ linked_list *list_create()
     return list;
 }
 
-list_node *list_get_root(linked_list *list)
-{
+list_node *list_get_root(linked_list *list) {
     if (list)
         return list->root;
     else
         return NULL;
 }
 
-list_node *list_push_front(linked_list *list, void *data)
-{
+list_node *list_push_front(linked_list *list, void *data) {
     list_node *tmp = NULL;
 
     if (list == NULL)
         return NULL;
 
     tmp = malloc(sizeof(list_node));
-    if (tmp)
-    {
+    if (tmp) {
         list_node *root = list_get_root(list);
 
         memset(tmp, 0, sizeof(*tmp));
         tmp->data = data;
 
-        if (root)
-        {
+        if (root) {
             list->root = tmp;
             tmp->next = root;
-        }
-        else
-        {
+        } else {
             list->root = tmp;
         }
     }
@@ -63,8 +65,7 @@ list_node *list_push_front(linked_list *list, void *data)
     return tmp;
 }
 
-list_node *list_insert_before(linked_list *list, list_node *pos, void *data)
-{
+list_node *list_insert_before(linked_list *list, list_node *pos, void *data) {
     list_node *tmp = NULL;
     list_node *pre = NULL;
 
@@ -75,8 +76,7 @@ list_node *list_insert_before(linked_list *list, list_node *pos, void *data)
         return list_push_front(list, data);
 
     pre = list_get_root(list);
-    while (pre->next != pos)
-    {
+    while (pre->next != pos) {
         pre = pre->next;
 
         if (!pre)
@@ -84,8 +84,7 @@ list_node *list_insert_before(linked_list *list, list_node *pos, void *data)
     }
 
     tmp = malloc(sizeof(list_node));
-    if (tmp)
-    {
+    if (tmp) {
         memset(tmp, 0, sizeof(*tmp));
 
         tmp->data = data;
@@ -96,8 +95,7 @@ list_node *list_insert_before(linked_list *list, list_node *pos, void *data)
     return tmp;
 }
 
-list_node *list_insert_after(linked_list *list, list_node *pos, void *data)
-{
+list_node *list_insert_after(linked_list *list, list_node *pos, void *data) {
     if ((list == NULL) || (pos == NULL))
         return NULL;
 
@@ -107,32 +105,27 @@ list_node *list_insert_after(linked_list *list, list_node *pos, void *data)
         return list_insert_before(list, pos->next, data);
 }
 
-list_node *list_push_back(linked_list *list, void *data)
-{
+list_node *list_push_back(linked_list *list, void *data) {
     list_node *tmp = NULL;
 
     if (list == NULL)
         return NULL;
 
     tmp = malloc(sizeof(list_node));
-    if (tmp)
-    {
+    if (tmp) {
         list_node *root = list_get_root(list);
 
         memset(tmp, 0, sizeof(*tmp));
         tmp->data = data;
 
-        if (root)
-        {
+        if (root) {
             list_node *current = root;
 
             while (current->next != NULL)
                 current = current->next;
 
             current->next = tmp;
-        }
-        else
-        {
+        } else {
             list->root = tmp;
         }
     }
@@ -140,8 +133,7 @@ list_node *list_push_back(linked_list *list, void *data)
     return tmp;
 }
 
-void list_dispose(linked_list **list, void (*free_content)(void *))
-{
+void list_dispose(linked_list **list, void (*free_content)(void *)) {
     list_node *current = NULL;
 
     if ((list == NULL) || (*list == NULL))
@@ -149,8 +141,7 @@ void list_dispose(linked_list **list, void (*free_content)(void *))
 
     current = (*list)->root;
 
-    while (current != NULL)
-    {
+    while (current != NULL) {
         list_node *next = current->next;
 
         if (free_content != NULL)
--- linked_list.h
+++ linked_list.h
@@ -1,13 +1,22 @@
-/******************************************************************************
+/*
+ * Copyright (c) 2012
  *
- *  Linked List library (libll.h)
- *  ---
- *  Library interface
+ * This file is part of dcpu16tools
  *
- *  Created: 05.01.2011 14:54:16
- *  Author:  Lukas Niederbremer
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- ******************************************************************************/
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 #ifndef LIBLL_H
 #define LIBLL_H
 
@@ -23,27 +32,25 @@ typedef struct list_node list_node;
 
 typedef linked_list list;
 
-struct linked_list
-{
+struct linked_list {
     list_node *root;
     
     size_t length;
 };
 
-struct list_node
-{
+struct list_node {
     void *data;
 
     list_node *next;
 };
 
 linked_list *list_create();
-list_node *list_get_root(linked_list *);
-list_node *list_push_front(linked_list *, void *);
-list_node *list_push_back(linked_list *, void *);
-list_node *list_insert_before(linked_list *, list_node *, void *);
-list_node *list_insert_after(linked_list *, list_node *, void *);
-void list_dispose(linked_list **, void (*)(void *));
+list_node *list_get_root(linked_list*);
+list_node *list_push_front(linked_list*, void*);
+list_node *list_push_back(linked_list*, void*);
+list_node *list_insert_before(linked_list*, list_node*, void *);
+list_node *list_insert_after(linked_list*, list_node*, void *);
+void list_dispose(linked_list**, void (*)(void*));
 
 
 /*
